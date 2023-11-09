7416b861ede9516df53dc350df36427f7a70db4c
matheus <matheus.mmn@hotmail.com>
2023-11-09 13:25:51 -0300

    ajujstes

--- leitura.js
+++ leitura.js
@@ -1,25 +1,47 @@
 $(window).on("load", function() {
     let tecnico = sessionStorage.getItem('tec')
-    $.get("http://alucomtec.pythonanywhere.com/producao/", function(e) {
-        let meus = e.filter(x => x.tecnico == tecnico)
-        console.log(meus)
-        var tabela = document.getElementById("registros")
-        tabela.innerHTML = ""
 
-        meus.forEach(i => {
-            tabela.innerHTML += `
-                <tr>
-                    <td>${i.tecnico}</td>
-                    <td>${i.patrimonio}</td>
-                    <td>${i.orgao}</td>
-                    <td>${i.equipamento}</td>
-                    <td>${i.defeito}</td>
-                    <td>${i.servico_realizado}</td>
-                    <td>${i.verificacao}</td>
-                    <td>${i.status}</td>
-                    <td>${i.observacao}</td>
-                </tr>
-            `
-        });
-    })
+
+    $.ajax({
+        url: 'http://alucomtec.pythonanywhere.com/producao/',  // Use HTTPS aqui
+        method: 'GET',
+        dataType: 'json',
+        success: function(data) {
+            console.log(e)
+        },
+        error: function(error) {
+            // Manipule erros
+        }
+    });
+
+
+
+
+
+
+
+
+
+
+    // $.get("http://alucomtec.pythonanywhere.com/producao/", function(e) {
+    //     let meus = e.filter(x => x.tecnico == tecnico)
+    //     console.log(meus)
+    //     var tabela = document.getElementById("registros")
+    //     tabela.innerHTML = ""
+    //     meus.forEach(i => {
+    //         tabela.innerHTML += `
+    //             <tr>
+    //                 <td>${i.tecnico}</td>
+    //                 <td>${i.patrimonio}</td>
+    //                 <td>${i.orgao}</td>
+    //                 <td>${i.equipamento}</td>
+    //                 <td>${i.defeito}</td>
+    //                 <td>${i.servico_realizado}</td>
+    //                 <td>${i.verificacao}</td>
+    //                 <td>${i.status}</td>
+    //                 <td>${i.observacao}</td>
+    //             </tr>
+    //         `
+    //     });
+    // })
 })
