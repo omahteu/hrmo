010f2e13e2bd8cfa7c1e48ed524fa295e44c4d87
matheus <matheus.mmn@hotmail.com>
2023-11-10 10:44:29 -0300

    ajustes

--- leitura.js
+++ leitura.js
@@ -1,47 +1,26 @@
-$(window).on("load", function() {
-    let tecnico = sessionStorage.getItem('tec')
-
-
-    $.ajax({
-        url: 'http://alucomtec.pythonanywhere.com/producao/',  // Use HTTPS aqui
-        method: 'GET',
-        dataType: 'json',
-        success: function(data) {
-            console.log(e)
-        },
-        error: function(error) {
-            // Manipule erros
-        }
-    });
-
-
-
-
-
-
-
-
-
-
-    // $.get("http://alucomtec.pythonanywhere.com/producao/", function(e) {
-    //     let meus = e.filter(x => x.tecnico == tecnico)
-    //     console.log(meus)
-    //     var tabela = document.getElementById("registros")
-    //     tabela.innerHTML = ""
-    //     meus.forEach(i => {
-    //         tabela.innerHTML += `
-    //             <tr>
-    //                 <td>${i.tecnico}</td>
-    //                 <td>${i.patrimonio}</td>
-    //                 <td>${i.orgao}</td>
-    //                 <td>${i.equipamento}</td>
-    //                 <td>${i.defeito}</td>
-    //                 <td>${i.servico_realizado}</td>
-    //                 <td>${i.verificacao}</td>
-    //                 <td>${i.status}</td>
-    //                 <td>${i.observacao}</td>
-    //             </tr>
-    //         `
-    //     });
-    // })
+$(document).on("change", "#tecnico", function() {
+    let tecnico = $(this).val()
+    let data = $("#dia").val()
+    var dataFormatada = data.split('-').reverse().join('/');
+    console.log(tecnico, data)
+    $.get("dados_api.json", function(e) {
+        let meus = e.filter(x => x.tecnico == tecnico && x.dia == dataFormatada)
+        var tabela = document.getElementById("registros")
+        tabela.innerHTML = ""
+        meus.forEach(i => {
+            tabela.innerHTML += `
+                <tr>
+                    <td>${i.tecnico}</td>
+                    <td>${i.patrimonio}</td>
+                    <td>${i.orgao}</td>
+                    <td>${i.equipamento}</td>
+                    <td>${i.defeito}</td>
+                    <td>${i.servico_realizado}</td>
+                    <td>${i.verificacao}</td>
+                    <td>${i.status}</td>
+                    <td>${i.observacao}</td>
+                </tr>
+            `
+        });
+    })
 })
